ebf0b5dea87e867fd06d1e0bf6e2cadf59e0fd04
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-05-28 22:03:05 -0300

    Create database connection

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,30 @@ const port = 3333 || process.env.PORT;
 app.use(cors());
 app.use(bodyParser.json());
 
+const databasePath = './database/database.db';
+const sqliteRule = sqlite3.OPEN_READWRITE;
+let database;
+
+function openConnection() {
+	database = new sqlite3.Database(databasePath, sqliteRule, error => {
+		if (error) {
+			return console.error(error.message);
+		}
+  	
+  	console.log('Database connected.');
+	});
+}
+
+function closeConnection() {
+	database.close(error => {
+		if (error) {
+			return console.error(error.message);
+		}
+  	
+  	console.log('Database closed.');
+	});
+}
+
 app.listen(port, () => {
 	console.log('Backend is running at localhost:' + port);
 });
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -13,9 +13,3 @@ void main(){
     )
 	);
 }
-
-// [ ] Criar tela "signup.dart"
-// [ ] Criar navegação entre "signin" e "signup"
-// [ ] Criar servidor com Node.js e SQlite
-// [ ] Consumir a API com JavaScript pra ver se tá funcionando direito
-// [ ] Consumir a API com Flutter
