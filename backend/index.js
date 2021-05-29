d8fa10728395f2004654cfb44229a1171d379715
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-05-29 11:19:17 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const port = 3333 || process.env.PORT;
+
+const config = require('./config.js');
+const authenticate = require('./controllers/authenticate.js');
+const register = require('./controllers/register.js');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const databasePath = './database/database.db';
-const sqliteRule = sqlite3.OPEN_READWRITE;
-let database;
-
-function openConnection() {
-	database = new sqlite3.Database(databasePath, sqliteRule, error => {
-		if (error) {
-			return console.error(error.message);
-		}
-  	
-  	console.log('Database connected.');
-	});
-}
-
-function closeConnection() {
-	database.close(error => {
-		if (error) {
-			return console.error(error.message);
-		}
-  	
-  	console.log('Database closed.');
-	});
-}
+app.post('/signin', authenticate);
+app.post('/signup', register);
 
 app.listen(port, () => {
 	console.log('Backend is running at localhost:' + port);
+	config.createUsersTable();
 });
