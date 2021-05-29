577fc17d2d213d1206159c2692a4e819a63077fa
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-05-29 11:57:13 -0300

    Add signup feature

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -1,9 +1,25 @@
 const config = require('../config.js');
 
 module.exports = function (request, response) {
-	//config.openConnection();
+	config.openConnection();
 
-	
+	const sql = 'INSERT INTO users(email, password, name) VALUES(?, ?, ?)';
+	const { email, password, name } = request.body;
 
-	//config.closeConnection();
+	config.database.run(sql, [email, password, name], error => {
+		if (error) {
+			return response.json({
+				ok: false,
+				message: 'Error: ' + error.message
+			});
+		}
+
+		return response.json({
+			ok: true,
+			data: request.body,
+			message: 'User registered successfully'
+		});
+	});
+
+	config.closeConnection();
 }
