bf2fbe7ab5f0e9821ba9f5088d5e4f4c62cca8a6
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-05-29 12:12:51 -0300

    Add authentication feature

--- backend/controllers/authenticate.js
+++ backend/controllers/authenticate.js
@@ -1,9 +1,34 @@
 const config = require('../config.js');
 
 module.exports = function (request, response) {
-	//config.openConnection();
+	config.openConnection();
 
-	
+	const sql = 'SELECT * FROM users WHERE email = ? AND password = ?';
+	const { email, password } = request.body;
 
-	//config.closeConnection();
+	config.database.get(sql, [email, password], (error, row) => {
+		if (error) {
+			return response.json({
+				ok: false,
+				status: 0,
+				message: 'Error: ' + error.message
+			});
+		}
+
+		if (row) {
+			return response.json({
+				ok: true,
+				status: 1,
+				message: 'Authenticated successfully'
+			});
+		}
+
+		return response.json({
+			ok: true,
+			status: 2,
+			message: 'Email and/or password don\'t match'
+		});
+	});
+
+	config.closeConnection();
 }
