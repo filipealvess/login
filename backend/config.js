2715b99aa50d4d8aa4bd93d97ed5b3444bcc9e3a
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-05-29 11:55:07 -0300

    Update config file structure

--- backend/config.js
+++ backend/config.js
@@ -1,41 +1,34 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let database;
-
-function openConnection() {
-	database = new sqlite3.Database('./database/database.db', sqlite3.OPEN_READWRITE, error => {
-		if (error) {
-			return console.error(error.message);
-		}
-  	
-  	console.log('Database connected.');
-	});
-}
-
-function createUsersTable() {
-	openConnection();
-	database.run(`
-		CREATE TABLE IF NOT EXISTS users(
-			email text PRIMARY KEY,
-			password text NOT NULL,
-			name text
-		);
-	`);
-	closeConnection();
-}
-
-function closeConnection() {
-	database.close(error => {
-		if (error) {
-			return console.error(error.message);
-		}
-  	
-  	console.log('Database closed.');
-	});
-}
-
 module.exports = {
-	openConnection,
-	closeConnection,
-	createUsersTable
+	database: null,
+	openConnection() {
+		this.database = new sqlite3.Database('./database/database.db', sqlite3.OPEN_READWRITE, error => {
+			if (error) {
+				return console.error(error.message);
+			}
+	  	
+	  	console.log('Database connected.');
+		});
+	},
+	closeConnection() {
+		this.database.close(error => {
+			if (error) {
+				return console.error(error.message);
+			}
+	  	
+	  	console.log('Database closed.');
+		});
+	},
+	createUsersTable() {
+		this.openConnection();
+		this.database.run(`
+			CREATE TABLE IF NOT EXISTS users(
+				email text PRIMARY KEY,
+				password text NOT NULL,
+				name text
+			);
+		`);
+		this.closeConnection();
+	}
 }
