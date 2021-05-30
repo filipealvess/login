95bd78c4fd2ed41419c2903c1252248d42266c2a
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-05-30 13:43:44 -0300

    Create back button

--- mobile/lib/screens/dashboard.dart
+++ mobile/lib/screens/dashboard.dart
@@ -4,14 +4,41 @@ class Dashboard extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
 		return Center(
-			child: Text(
-				'Dashboard',
-				style: const TextStyle(
-			    color: Colors.black,
-			    fontWeight: FontWeight.bold,
-			    fontSize: 18.0,
-			    decoration: TextDecoration.none
-			  )
+			child: Padding(
+				padding: const EdgeInsets.all(10.0),
+				child: Column(
+					mainAxisAlignment: MainAxisAlignment.center,
+					children: [
+						Row(
+							children: [
+								TextButton(
+									onPressed: () {
+										Navigator.of(context).pushReplacementNamed('/');
+									},
+									child: Row(
+										children: [
+											Icon(
+									      Icons.arrow_back_rounded,
+									      semanticLabel: 'Seta para a esquerda',
+									    ),
+											Text('SAIR')
+										]
+									)
+								)
+							]
+						),
+						SizedBox(height: 15),
+						Text(
+							'Dashboard',
+							style: const TextStyle(
+						    color: Colors.black,
+						    fontWeight: FontWeight.bold,
+						    fontSize: 18.0,
+						    decoration: TextDecoration.none
+						  )
+						),
+					]
+				)
 			)
 		);
 	}
