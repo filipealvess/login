e7d3596cec11ebf1bc6676e0079a7ecf7f9365f1
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-05-30 11:05:24 -0300

    Update register controller feedbacks

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -8,14 +8,24 @@ module.exports = function (request, response) {
 
 	config.database.run(sql, [email, password, name], error => {
 		if (error) {
+			if (error.message === 'SQLITE_CONSTRAINT: UNIQUE constraint failed: users.email') {
+				return response.json({
+					ok: false,
+					status: 1,
+					message: 'Email error: this email already exists in the database'
+				});
+			}
+
 			return response.json({
 				ok: false,
-				message: 'Error: ' + error.message
+				status: 0,
+				message: 'Unknown error: ' + error.message
 			});
 		}
 
 		return response.json({
 			ok: true,
+			status: 2,
 			data: request.body,
 			message: 'User registered successfully'
 		});
