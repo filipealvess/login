ab51427c78ca19c2e89ca37e6aa104def1dc16b5
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-06-01 19:09:58 -0300

    Remove dashboard route

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import './screens/signin.dart';
 import './screens/signup.dart';
-import './screens/dashboard.dart';
 
 void main(){
 	runApp(
@@ -9,8 +8,7 @@ void main(){
     	initialRoute: '/',
     	routes: {
     		'/': (context) => Signin(),
-    		'/novo': (context) => Signup(),
-    		'/dashboard': (context) => Dashboard(),
+    		'/novo': (context) => Signup()
     	}
     )
 	);
